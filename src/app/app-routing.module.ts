73766de309ce306fbdc7a29041f378e4cf286783
Peter <pfreche@gmail.com>
2022-07-09 11:17:47 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BookmarksComponent} from "./bookmarks/bookmarks.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'bookmarks', component: BookmarksComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
